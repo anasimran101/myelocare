2d257fa06d4b3dc840e10ab904358a761d4a8137
Anas Imran <anaspunia@gmail.com>
2026-03-25 15:18:12 +0500

    edited run_project shell script
    added the custom strategy and partiiotner options in the script

--- scripts/run_project.sh
+++ scripts/run_project.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
 # Activate virtual environment
-source /home/anas/python_venvs/pytorch-gpu/bin/activate
+source /home/lab/code/myelocare_env/bin/activate
 
 # Parameters
 EPOCHS_LIST=(1 5 10 25 50 75)
 ROUNDS_LIST=(5 10 15 20)
 LR_LIST=(0.001)
+STRATEGIES=("FedProx")
+PARTITIONER="noniid"
 
 # Create logs directory once
 mkdir -p logs
 
 # Loop over parameters
-for E in "${EPOCHS_LIST[@]}"; do
-  for R in "${ROUNDS_LIST[@]}"; do
-    for LR in "${LR_LIST[@]}"; do
-      
-      if [ $E -lte 10 ] && [ $R -lte 10 ]; then
-        continue
-      fi
+for SR in "${STRATEGIES[@]}"; do
+  for E in "${EPOCHS_LIST[@]}"; do
+    for R in "${ROUNDS_LIST[@]}"; do
+      for LR in "${LR_LIST[@]}"; do
 
-      LOG="logs/e${E}_r${R}_lr${LR}.log"
+        LOG="logs/${SR}_e${E}_r${R}_lr${LR}.log"
 
-      echo "Running E=$E R=$R LR=$LR ..."
-      flwr run . local-simulation-gpu \
-        --run-config="num-server-rounds=${R} local-epochs=${E} learning-rate=${LR} batch-size=1 num-clients=3 fraction-evaluate=0.2 run-partitioner=0" \
-        > "$LOG" 2>&1
+        echo "Running STRATEGY=$SR E=$E R=$R LR=$LR ..."
 
-      echo "Completed: $LOG"
+        flwr run . local-simulation-gpu \
+          --run-config="num-server-rounds=${R} local-epochs=${E} learning-rate=${LR} strategy=${SR} partitioner=${PARTITIONER} batch-size=1 num-clients=3 fraction-evaluate=0.2" \
+          > "$LOG" 2>&1
+
+        echo "Completed: $LOG"
+      done
     done
   done
 done
